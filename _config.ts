a28453471257a38bdcb371ff4a71adc49022a748
Lex Feathers <lex@gameaudio.ca>
2024-09-16 20:53:56 -0400

    Trying another feed config

--- _config.ts
+++ _config.ts
@@ -6,6 +6,7 @@ import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import feed from "lume/plugins/feed.ts";
 import _cleancss from 'https://deno.land/x/lume_cleancss@v0.2.0/mod.ts';
+import { underline } from "lume/deps/colors.ts";
 
 const site = lume();
 site.use(nunjucks());
@@ -23,25 +24,26 @@ site.use(date({
 }));
 site.use(metas({/* Options */}));
 site.use(feed({
-  output: ["/feed.rss", "/feed.json"], // The file or files that must be generated
-  query: "type=post", // Select only pages of type=post
-  sort: "date=desc", // To sort by data in ascendent order
-  limit: 10, // To show only the 10 first results
+  output: ["/feed.rss"],
+
+  query: "type=posts",
+  sort: "date=desc",
+
   info: {
-    title: "=site_name", // The feed title
-    description: "=site.description", // The feed subtitle
-    published: new Date(), // The publishing date
-    lang: "en", // The language of the feed
-    generator: true, // Set `true` to automatically generate the "Lume {version}"
+    title: "Void Femmes",
+    description:
+      "music and ideas by Lex Feathers",
+    lang: "en",
+    generator: false,
   },
   items: {
-    title: "=title", // The title of every item
-    description: "=excerpt", // The description of every item
-    published: "=date", // The publishing date of every item
-    updated: undefined, // The last update of every item
-    content: "=children", // The content of every item
-    lang: "=lang", // The language of every item
-    image: "=cover", // The image of the item
+    title: "=title",
+    description: "=description",
+    published: "=date",
+    updated: undefined,
+    content: "=children",
+    lang: "en",
+    image: "=image", // The image of the item
   },
 }));
 
